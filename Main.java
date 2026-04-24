f198e989cafbe6552753c02ed3c5eae7a84a2a52
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 11:06:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,27 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        // create list of doctors
+        // Doctors
         ArrayList<MedicalProfessional> doctors = new ArrayList<>();
-
-        // add different doctors
         doctors.add(new Surgeon(101, "John", 1));
         doctors.add(new Nurse(102, "Sarah", 1));
         doctors.add(new Cardiologist(103, "Mike", 0));
 
-        // loop through list
-        for (MedicalProfessional d : doctors){
-            d.displayInfo();     // show info
-            d.performAction();  // show action
+        System.out.println("=== Doctors ===");
+        for (MedicalProfessional d : doctors) {
+            d.displayInfo();
+            d.performAction();
             System.out.println();
         }
-        
+
+        // Patients
+        ArrayList<Patient> patients = new ArrayList<>();
+        patients.add(new PediatricPatient("Liam", 201, 8, "Fever"));
+        patients.add(new EmergencyPatient("Carlos", 203, 30, "Accident"));
+
+        System.out.println("=== Patients ===");
+        for (Patient p : patients) {
+            System.out.println(p.getInfo());
+        }
     }
 }
