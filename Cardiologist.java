7fd1292b886d99beb9fd5ce634a5302ea192ba5d
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 11:37:24 -0400

    Update Cardiologist.java

--- Cardiologist.java
+++ Cardiologist.java
@@ -2,18 +2,18 @@ public class Cardiologist extends MedicalProfessional {
 
     public String specialty;
 
-    // Creates a cardiologist and sets the specialty
+    // this creates a   cardiologist and sets the specialty
     public Cardiologist(int docID, String docName, int availability) {
         super(docID, docName, availability);
         specialty = "Cardiologist";
     }
 
-    // Shows what the cardiologist does
+    // shows what the cardiologist do
     public void performAction() {
         System.out.println(getDocName() + " checks heart-related problems.");
     }
 
-    // Prepares cardiologist information for saving
+    // prepares cardiologist information for saving
     //txtprep Compile
     public String txtPrep() {
         return getDocID() + "," + getDocName() + "," + specialty + "," + availability;
