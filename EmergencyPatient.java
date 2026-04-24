12f63ea7e993c434dda8eb705d388c5df4087fd8
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 10:17:36 -0400

    Update EmergencyPatient.java
    Added comments to clarify constructor and method functionality.

--- EmergencyPatient.java
+++ EmergencyPatient.java
@@ -1,12 +1,14 @@
 public class EmergencyPatient extends Patient {
 
+    // Creates an emergency patient and assigns a surgeon
     public EmergencyPatient(String name, int id, int age, String medicalIssue) {
         super(name, id, age, medicalIssue);
         this.patientType = 3;
         this.doctorType = "Surgeon";
-        this.condition = "Emergency"; // always emergency
+        this.condition = "Emergency"; //always emergency
     }
 
+    // Returns emergency patient information
     @Override
     public String getInfo() {
         return "[Emergency] " + super.getInfo();
