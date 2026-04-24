c1904ac7d396fca66a5f2588da190f0bddd45328
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 10:15:52 -0400

    ...
    Added comments to clarify constructor and method functionality.

--- CardiologyPatient.java
+++ CardiologyPatient.java
@@ -1,11 +1,13 @@
 public class CardiologyPatient extends Patient {
 
+    // Creates a cardiology patient and assigns a cardiologist
     public CardiologyPatient(String name, int id, int age, String medicalIssue) {
         super(name, id, age, medicalIssue);
         this.patientType = 2;
         this.doctorType = "Cardiologist";
     }
 
+    // Returns cardiology patient information
     @Override
     public String getInfo() {
         return "[Cardiology] " + super.getInfo();
