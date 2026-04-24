e04d6132d46f74eea51c1cc9e9cb489e65a8a005
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 10:18:57 -0400

    Update FamilyDoctor.java
    Added comments to clarify methods and functionality.

--- FamilyDoctor.java
+++ FamilyDoctor.java
@@ -1,16 +1,20 @@
 public class FamilyDoctor extends MedicalProfessional {
-    
+
     public String specialty;
 
-    public FamilyDoctor(int docID, String docName, int availability){
+    //  family doctor and sets the specialty
+    public FamilyDoctor(int docID, String docName, int availability) {
         super(docID, docName, availability);
         specialty = "Family Doctor";
     }
 
-    public void performAction(){
+    // shows what the family doctor does
+    public void performAction() {
+        System.out.println(getDocName() + " treats general health problems.");
     }
 
-    public String txtPrep(){
-        return docID + "," + docName + "," + specialty + "," + availability
+    // prepares family doctor information for saving
+    public String txtPrep() {
+        return getDocID() + "," + getDocName() + "," + specialty + "," + availability;
     }
 }
