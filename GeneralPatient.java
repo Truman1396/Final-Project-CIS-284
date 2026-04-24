66c4a02a8ab32364b2af42c819ec69bc815b1817
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 10:19:45 -0400

    Update GeneralPatient.java

--- GeneralPatient.java
+++ GeneralPatient.java
@@ -1,11 +1,13 @@
 public class GeneralPatient extends Patient {
 
+    //  general patient and assigns a family doctor
     public GeneralPatient(String name, int id, int age, String medicalIssue) {
         super(name, id, age, medicalIssue);
         this.patientType = 4;
-        this.doctorType = "Family Doctor"; // could also be Nurse
+        this.doctorType = "Family Doctor";
     }
 
+    // returns the general patient information
     @Override
     public String getInfo() {
         return "[General] " + super.getInfo();
