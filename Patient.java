5c160987a003ce89562711d7defc89186a18c6f1
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 11:12:58 -0400

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,34 +1,34 @@
-// Base class for all patients
 public class Patient {
+
     protected String name;
     protected int id;
     protected int age;
     protected String medicalIssue;
     protected String condition;
 
-    protected int patientType;     // number for patient type
-    protected String doctorType;   // assigned doctor type
+    protected int patientType;
+    protected String doctorType;
 
-    // Constructor
+    // creates a patient with basic info
     public Patient(String name, int id, int age, String medicalIssue) {
         this.name = name;
         this.id = id;
         this.age = age;
         this.medicalIssue = medicalIssue;
-        this.condition = "Checkup"; // default condition
+        this.condition = "Checkup";
     }
 
-    // Update patient's condition
+    // uptade on patients conditions.
     public void updateCondition(String newCondition) {
         this.condition = newCondition;
     }
 
-    // Check if patient can be discharged
+    // makes sure if the patient is ready to leave.
     public boolean canBeDischarged() {
         return condition.equalsIgnoreCase("Discharge");
     }
 
-    // Get patient info
+    // returns all patient info as text
     public String getInfo() {
         return "Name: " + name +
                ", ID: " + id +
@@ -39,35 +39,32 @@ public class Patient {
                ", Doctor: " + doctorType;
     }
 
+    // Returns the patient's
+    //ID
     public int getId() {
         return id;
     }
 
-    // Check if a doctor is available for this patient
+    // thi checks if 
+    //needed doctor type is   available
     public boolean assignDoctorIfAvailable(int pediatricians,
-                                        int cardiologists,
-                                        int surgeons,
-                                        int familyDoctors,
-                                        int nurses) {
+                                           int cardiologists,
+                                           int surgeons,
+                                           int familyDoctors,
+                                           int nurses) {
 
-    // Check based on doctor type
-    if (doctorType.equals("Pediatrician") && pediatricians > 0) {
-        return true;
-    }
-    else if (doctorType.equals("Cardiologist") && cardiologists > 0) {
-        return true;
-    }
-    else if (doctorType.equals("Surgeon") && surgeons > 0) {
-        return true;
-    }
-    else if (doctorType.equals("Family Doctor") && familyDoctors > 0) {
-        return true;
-    }
-    else if (doctorType.equals("Nurse") && nurses > 0) {
-        return true;
-    }
+        if (doctorType.equals("Pediatrician") && pediatricians > 0) {
+            return true;
+        } else if (doctorType.equals("Cardiologist") && cardiologists > 0) {
+            return true;
+        } else if (doctorType.equals("Surgeon") && surgeons > 0) {
+            return true;
+        } else if (doctorType.equals("Family Doctor") && familyDoctors > 0) {
+            return true;
+        } else if (doctorType.equals("Nurse") && nurses > 0) {
+            return true;
+        }
 
-    // No doctor available
-    return false;
-}
+        return false;
+    }
 }
