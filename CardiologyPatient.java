5fa4c78fda9a432c14729667de9b643093280b7d
Dinhoron <Ronaldhuaman656@gmail.com>
2026-04-24 11:38:25 -0400

    Update CardiologyPatient.java

--- CardiologyPatient.java
+++ CardiologyPatient.java
@@ -1,13 +1,15 @@
 public class CardiologyPatient extends Patient {
 
-    // Creates a cardiology patient and assigns a cardiologist
+    // creates a cardiology patient and 
+    //    assigns a cardiologist
     public CardiologyPatient(String name, int id, int age, String medicalIssue) {
         super(name, id, age, medicalIssue);
         this.patientType = 2;
         this.doctorType = "Cardiologist";
     }
 
-    // Returns cardiology patient information
+    // returns cardiology 
+    //     patient information
     @Override
     public String getInfo() {
         return "[Cardiology] " + super.getInfo();
