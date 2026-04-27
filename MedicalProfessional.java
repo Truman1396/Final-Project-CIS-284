15a12be249b03b91f84c7de8f16597fae07d45a2
Truman1396 <th1396@messiah.edu>
2026-04-27 16:01:48 -0400

    Update MedicalProfessional.java

--- MedicalProfessional.java
+++ MedicalProfessional.java
@@ -3,56 +3,50 @@ public abstract class MedicalProfessional {
     private int docID;
     private String docName;
     public int availability;
+    public String specialty;
 
-    // empty constructor
-    public MedicalProfessional() {
+    public MedicalProfessional(){
     }
 
-    // sets the doctor's ID, name, and availability
-    public MedicalProfessional(int docID, String docName, int availability) {
+    public MedicalProfessional(int docID, String docName, String specialty, int availability){
+
         this.docID = docID;
         this.docName = docName;
+        this.specialty = specialty;
         this.availability = availability;
     }
 
-    // returns the doctor's ID
-    public int getDocID() {
+    public int getDocID(){
         return docID;
     }
 
-    // returns the doctor's name
-    public String getDocName() {
+    public String getDocName(){
         return docName;
     }
 
-    // prints if the doctor is available or not
-    public void isAvailable() {
-        if (availability == 1) {
+    public void isAvailable(){
+        if (availability == 1){
             System.out.println("Doctor " + docName + " is available");
-        } else {
+        } else{
             System.out.println("Doctor " + docName + " is not available");
         }
     }
 
-    // changes availability between available and not available
-    public void changeAvailable() {
-        if (availability == 1) {
+    public void changeAvailable(){
+        if (availability == 1){
             availability = 0;
-        } else if (availability == 0) {
+        } else if (availability == 0){
             availability = 1;
         }
     }
 
-    // each doctor class must define its own action
     public abstract void performAction();
 
-    // each doctor class must prepare its info for text storage
     public abstract String txtPrep();
 
-    // prints the doctor's basic information
-    public void displayInfo() {
+    public void displayInfo(){
         System.out.println("Doctor ID: " + docID);
-        System.out.println("Doctor Name: " + docName);
+        System.out.println("Doctor Name: " + docName + " (" + specialty + ")");
         this.isAvailable();
     }
 }
