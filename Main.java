8149a547779936b2e728b46297a07563053d2e81
Truman1396 <th1396@messiah.edu>
2026-04-27 16:54:37 -0400

    Patient Admission

--- Main.java
+++ Main.java
@@ -10,12 +10,82 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        int choice = 0;
 
         ArrayList<MedicalProfessional> doctors = new ArrayList<>();
         loadDoctorsFromFile(doctors);
         ArrayList<Patient> patients = new ArrayList<>();
         loadPatientsFromFile(patients);
 
+
+        do{
+
+            System.out.println();
+            System.out.println("====Select Action====");
+            System.out.println("1.) Admit a Patient \n2.) Assign Patient a Doctor \n3.) Update Patient Condition \n4.) Discharge Patient \n5.) Display Patient Info \n6.) Exit");
+            System.out.println();
+            System.out.print("Enter the action you would like to perform: ");
+            choice = Integer.parseInt(in.nextLine());
+            System.out.println();
+
+            if (choice == 1){
+                System.out.print("Patient ID: ");
+                int tempID = Integer.parseInt(in.nextLine());
+                System.out.print("Patient Name: ");
+                String tempName = in.nextLine();
+                System.out.print("Patient Age: ");
+                int tempAge = Integer.parseInt(in.nextLine());
+                System.out.println("---Select Medical Issue---");
+                System.out.println("1.) Chest Pain \n2.) Broken Bone \n3.) Other");
+                System.out.println();
+                System.out.print("Enter Patient's Medical Issue: ");
+                String tempMedicalIssue = "Other";
+                int selection = Integer.parseInt(in.nextLine());
+                if ((selection >= 1) && (selection <= 3)){
+                    if (tempAge <= 18){
+                        if (selection == 1){
+                            tempMedicalIssue = "Chest Pain";
+                        } else if (selection == 2){
+                            tempMedicalIssue = "Broken Bone";
+                        } else if (selection == 3){
+                            tempMedicalIssue = "Other";
+                        }
+                        patients.add(new PediatricPatient(tempName, tempID, tempAge, tempMedicalIssue));
+                    } else if (selection == 1){
+                        tempMedicalIssue = "Chest Pain";
+                        patients.add(new CardiologyPatient(tempName, tempID, tempAge, tempMedicalIssue));
+                    } else if (selection == 2){
+                        tempMedicalIssue = "Broken Bone";
+                        patients.add(new EmergencyPatient(tempName, tempID, tempAge, tempMedicalIssue));
+                    } else if (selection == 3){
+                        tempMedicalIssue = "Other";
+                        patients.add(new GeneralPatient(tempName, tempID, tempAge, tempMedicalIssue));
+                    }
+                } else {
+                    System.out.println("Incorrect Information Provided");
+                }
+            } else if (choice == 2){
+                //Assign a doctor
+            } else if (choice == 3){
+                //Update Condition
+            } else if (choice == 4){
+                //Discharge Patient
+            } else if (choice == 5){
+                System.out.println("---Patient Info---");
+                for (Patient p : patients){
+                    System.out.println(p.getInfo());
+                }
+            } else if (choice == 6){
+                System.out.println("Program Exited");
+
+            //Catch everything else
+            } else {
+                System.out.println("Not a valid option. Please select a valid action.");
+                System.out.println();
+            }
+
+
+        } while (choice != 6);
 
         saveDoctorsToFile(doctors);
         savePatientsToFile(patients);
