ee6855b487e85853f00fe0d408bfd827a6add8ad
Truman1396 <th1396@messiah.edu>
2026-04-28 14:52:05 -0400

    Update Nurse.java

--- Nurse.java
+++ Nurse.java
@@ -1,13 +1,16 @@
 public class Nurse extends MedicalProfessional {
 
+    //Constructor
     public Nurse(int docID, String docName, String specialty, int availability){
-
         super(docID, docName, specialty, availability);
     }
 
+    //Nurse Action (abstract method)
     public void performAction(){
+        System.out.println(this.getDocName() + " confirmed patient's availability for discharge");
     }
 
+    //Prepare info for file writing
     public String txtPrep(){
         return this.getDocID() + "," + this.getDocName() + "," + specialty + "," + availability;
     }
