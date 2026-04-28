b2ce9832a2d2d086f65a22ef39728d9fe9bfdafa
Truman1396 <th1396@messiah.edu>
2026-04-28 14:50:37 -0400

    Update Cardiologist.java

--- Cardiologist.java
+++ Cardiologist.java
@@ -1,13 +1,16 @@
 public class Cardiologist extends MedicalProfessional {
 
+    //Constructor
     public Cardiologist(int docID, String docName, String specialty, int availability){
-
         super(docID, docName, specialty, availability);
     }
 
+    //Cardiologist action (abstract method)
     public void performAction(){
+        System.out.println(this.getDocName() + " ran an EKG and prescribed blood pressure medicine");
     }
 
+    //Preparing info for file writing
     public String txtPrep(){
         return this.getDocID() + "," + this.getDocName() + "," + specialty + "," + availability;
     }
