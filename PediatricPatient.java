389ced45656f7b064b2adfaeaa8c32476c574373
Truman1396 <th1396@messiah.edu>
2026-04-28 11:57:52 -0400

    Update PediatricPatient.java

--- PediatricPatient.java
+++ PediatricPatient.java
@@ -1,9 +1,7 @@
 public class PediatricPatient extends Patient {
 
-    public PediatricPatient(String name, int id, int age, String medicalIssue) {
-        super(name, id, age, medicalIssue);
-        this.patientType = 1;             // assign type number
-        this.doctorType = "Pediatrician"; // assign doctor
+    public PediatricPatient(String name, int id, int age, String medicalIssue, String condition, String assignedDoctor) {
+        super(name, id, age, medicalIssue, condition, assignedDoctor);
     }
 
     @Override
@@ -12,6 +10,6 @@ public class PediatricPatient extends Patient {
     }
 
     public String txtPrep(){
-        return name + "," + this.getId() + "," + age + "," + medicalIssue + "," + patientType + "," + doctorType;
+        return name + "," + this.getId() + "," + age + "," + medicalIssue + "," + condition + "," + assignedDoctor;
     }
 }
