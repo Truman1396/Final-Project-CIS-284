bb3de282615562297c3dac37650c5c45e8b4c038
Truman1396 <th1396@messiah.edu>
2026-04-28 14:47:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+//Import statements
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,26 +10,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Creates scanner for user input
         Scanner in = new Scanner(System.in);
         int choice = 0;
 
+        //Creates arraylists and populates them with info from text files
         ArrayList<MedicalProfessional> doctors = new ArrayList<>();
         loadDoctorsFromFile(doctors);
         ArrayList<Patient> patients = new ArrayList<>();
         loadPatientsFromFile(patients);
 
 
+        //User interaction (always runs at least once)
         do{
 
+            //Selection screen: choose an option
             System.out.println();
             System.out.println("====Select Action====");
-            System.out.println("1.) Admit a Patient \n2.) Assign Patient a Doctor \n3.) Discharge Patient \n4.) Display Patient Info \n5.) Exit");
+            System.out.println("1.) Admit a Patient \n2.) Assign Patient a Doctor \n3.) Discharge Patient \n4.) Display Patient Info \n5.) Display Doctor Info\n6.) Exit");
             System.out.println();
             System.out.print("Enter the action you would like to perform: ");
             choice = Integer.parseInt(in.nextLine());
             System.out.println();
 
+
+            //Admit a patient
             if (choice == 1){
+                //Collecting patient information
                 System.out.print("Patient ID: ");
                 int tempID = Integer.parseInt(in.nextLine());
                 System.out.print("Patient Name: ");
@@ -42,6 +50,7 @@ public class Main {
                 String tempMedicalIssue = "Other";
                 int selection = Integer.parseInt(in.nextLine());
                 if ((selection >= 1) && (selection <= 3)){
+                    //Assigning patient to a subclass depending on medical needs
                     if (tempAge <= 18){
                         if (selection == 1){
                             tempMedicalIssue = "Chest Pain";
@@ -50,19 +59,19 @@ public class Main {
                         } else if (selection == 3){
                             tempMedicalIssue = "Other";
                         }
-                        patients.add(new PediatricPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", null));
+                        patients.add(new PediatricPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", "No Doctor"));
                         System.out.println("Patient " + tempID + " admitted to the pediatrics unit");
                     } else if (selection == 1){
                         tempMedicalIssue = "Chest Pain";
-                        patients.add(new CardiologyPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", null));
+                        patients.add(new CardiologyPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", "No Doctor"));
                         System.out.println("Patient " + tempID + " admitted to the cardiology unit");
                     } else if (selection == 2){
                         tempMedicalIssue = "Broken Bone";
-                        patients.add(new EmergencyPatient(tempName, tempID, tempAge, tempMedicalIssue, "Emergency", null));
+                        patients.add(new EmergencyPatient(tempName, tempID, tempAge, tempMedicalIssue, "Emergency", "No Doctor"));
                         System.out.println("Patient " + tempID + " admitted to the emergency unit");
                     } else if (selection == 3){
                         tempMedicalIssue = "Other";
-                        patients.add(new GeneralPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", null));
+                        patients.add(new GeneralPatient(tempName, tempID, tempAge, tempMedicalIssue, "Checkup", "No Doctor"));
                         System.out.println("Patient " + tempID + " admitted to the general unit");
                     } else {
                         System.out.println("Incorrect Information Provided");
@@ -70,9 +79,12 @@ public class Main {
                 } else {
                     System.out.println("Incorrect Information Provided");
                 }
+                System.out.println();
 
-                
+
+            //Assign Patient to Doctor
             } else if (choice == 2){
+                //Choosing patient
                 System.out.print("Please enter Patient's ID number: ");
                 int number = Integer.parseInt(in.nextLine());
                 if (findPatient(number, patients) != null){
@@ -81,7 +93,9 @@ public class Main {
                     System.out.println(tempPatient.getInfo());
                     System.out.println();
 
-                    if ((tempPatient.getAssignedDoctor()).equals(null)){
+                    //Assigning a correct and available doctor
+                    //Airlifts patient and removes from system if no doctor is available
+                    if ((tempPatient.getAssignedDoctor()).equals("No Doctor")){
                         if (tempPatient.getAge() <= 18){
                             for (MedicalProfessional p : doctors){
                                 if ((p.getSpecialty()).equals("Pediatrician")){
@@ -89,6 +103,7 @@ public class Main {
                                         System.out.println(p.getDocName() + " has been assigned to Patient " + tempPatient.getId());
                                         tempPatient.assignDoctor(p.getDocName());
                                         p.changeAvailable();
+                                        p.performAction();
                                         tempPatient.updateCondition("Available for Discharge");
                                         System.out.println("Patient " + tempPatient.getId() + " is available for discharge");
                                         break;
@@ -112,6 +127,7 @@ public class Main {
                                         System.out.println(p.getDocName() + " has been assigned to Patient " + tempPatient.getId());
                                         tempPatient.assignDoctor(p.getDocName());
                                         p.changeAvailable();
+                                        p.performAction();
                                         tempPatient.updateCondition("Available for Discharge");
                                         System.out.println("Patient " + tempPatient.getId() + " is available for discharge");
                                         break;
@@ -135,6 +151,7 @@ public class Main {
                                         System.out.println(p.getDocName() + " has been assigned to Patient " + tempPatient.getId());
                                         tempPatient.assignDoctor(p.getDocName());
                                         p.changeAvailable();
+                                        p.performAction();
                                         tempPatient.updateCondition("Available for Discharge");
                                         System.out.println("Patient " + tempPatient.getId() + " is available for discharge");
                                         break;
@@ -158,6 +175,7 @@ public class Main {
                                         System.out.println(p.getDocName() + " has been assigned to Patient " + tempPatient.getId());
                                         tempPatient.assignDoctor(p.getDocName());
                                         p.changeAvailable();
+                                        p.performAction();
                                         tempPatient.updateCondition("Available for Discharge");
                                         System.out.println("Patient " + tempPatient.getId() + " is available for discharge");
                                         break;
@@ -182,20 +200,62 @@ public class Main {
                     System.out.println("Patient not found: Please select another action");
                     System.out.println();
                 }
+                System.out.println();
 
 
+            //Discharge Patient
             } else if (choice == 3){
-                //Discharge Patient
+                //Choosing patient
+                //Discharing patient if able
+                System.out.print("Please enter Patient's ID number: ");
+                int number = Integer.parseInt(in.nextLine());
+                if (findPatient(number, patients) != null){
+                    Patient tempPatient = findPatient(number, patients);
+                    if ((tempPatient.getCondition()).equals("Available for Discharge")){
+                        System.out.println("Patient " + tempPatient.getId() + " is available for discharge: \n--Proceed?-- \n1.) Yes \n2.) No \n-> ");
+                        int decision = Integer.parseInt(in.nextLine());
+                        if (decision == 1){
+                            MedicalProfessional finalCheck = findDoctor("Nurse Thompson", doctors);
+                            finalCheck.performAction();
+                            System.out.println("Patient " + tempPatient.getId() + " has been discharged");
+                            MedicalProfessional switchDoc = findDoctor(tempPatient.getAssignedDoctor(), doctors);
+                            switchDoc.changeAvailable();
+                            switchDoc.checkAvailable();
+                            patients.remove(tempPatient);
+                        } else if (decision != 2){
+                            System.out.println("Incorrect Information");
+                        }
+                    } else {
+                        System.out.println("Patient " + tempPatient.getId() + " is not available for discharge");
+                    }
+                } else {
+                    System.out.println("Patient not found: Please select another action");
+                    System.out.println();
+                }
+                System.out.println();
 
 
+            //Display info of current patients
             } else if (choice == 4){
                 System.out.println("---Patient Info---");
                 for (Patient p : patients){
                     System.out.println(p.getInfo());
                 }
+                System.out.println();
 
 
+            //Display info of current doctors
             } else if (choice == 5){
+                System.out.println("---Doctor Info---");
+                for (MedicalProfessional d : doctors){
+                    d.displayInfo();
+                    System.out.println();
+                }
+                System.out.println();
+
+
+            //Exit Program
+            } else if (choice == 6){
                 System.out.println("Program Exited");
 
 
@@ -206,14 +266,15 @@ public class Main {
             }
 
 
-        } while (choice != 5);
+        } while (choice != 6);
 
+        //Save information to the text files
         saveDoctorsToFile(doctors);
         savePatientsToFile(patients);
     }
 
 
-    //Confirm a Patient Exists
+    //Confirm if a Patient Exists
     public static Patient findPatient(int id, ArrayList<Patient> patients){
         for (Patient pat : patients){
             if (pat.getId() == id){
@@ -223,7 +284,15 @@ public class Main {
         return null;
     }
 
-
+    //Find Doctor by Name
+    public static MedicalProfessional findDoctor(String docName, ArrayList<MedicalProfessional> doctors){
+        for (MedicalProfessional doc : doctors){
+            if ((doc.getDocName()).equals(docName)){
+                return doc;
+            }
+        }
+        return null;
+    }
 
 
 
