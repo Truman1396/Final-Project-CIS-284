ead8dda8483eeadde1ac59f5c4d07de783da50dd
Truman1396 <th1396@messiah.edu>
2026-04-28 14:51:23 -0400

    Update FamilyDoctor.java

--- FamilyDoctor.java
+++ FamilyDoctor.java
@@ -1,13 +1,16 @@
 public class FamilyDoctor extends MedicalProfessional {
 
+    //Constructor
     public FamilyDoctor(int docID, String docName, String specialty, int availability){
-
         super(docID, docName, specialty, availability);
     }
 
+    //Family Doctor Action (abstract method)
     public void performAction(){
+        System.out.println(this.getDocName() + " ran vitals and prescribed painkillers");
     }
 
+    //Prepare info for file writing
     public String txtPrep(){
         return this.getDocID() + "," + this.getDocName() + "," + specialty + "," + availability;
     }
