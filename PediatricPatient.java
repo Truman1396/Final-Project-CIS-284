9b3a8a2eec6419473355a608b0bb8b2eacde5136
Truman1396 <th1396@messiah.edu>
2026-04-28 14:56:41 -0400

    Update PediatricPatient.java

--- PediatricPatient.java
+++ PediatricPatient.java
@@ -1,14 +1,17 @@
 public class PediatricPatient extends Patient {
 
+    // creates pediatric patient (constructor)
     public PediatricPatient(String name, int id, int age, String medicalIssue, String condition, String assignedDoctor) {
         super(name, id, age, medicalIssue, condition, assignedDoctor);
     }
 
     @Override
+    // provides info
     public String getInfo() {
         return "[Pediatric] " + super.getInfo();
     }
 
+    //prepares info for file writing
     public String txtPrep(){
         return name + "," + this.getId() + "," + age + "," + medicalIssue + "," + condition + "," + assignedDoctor;
     }
