225cf654a57920e5e4800f9ee7e0787623de1ba9
Truman1396 <th1396@messiah.edu>
2026-04-28 14:53:11 -0400

    Update Surgeon.java

--- Surgeon.java
+++ Surgeon.java
@@ -1,13 +1,16 @@
 public class Surgeon extends MedicalProfessional {
     
+    //Constructor
     public Surgeon(int docID, String docName, String specialty, int availability){
-
         super(docID, docName, specialty, availability);
     }
 
+    //Surgeon action (abstract method)
     public void performAction(){
+        System.out.println(this.getDocName() + " performed a successful surgery, set a cast, and prescribed pain medication");
     }
 
+    //Prepare info for file writing
     public String txtPrep(){
         return this.getDocID() + "," + this.getDocName() + "," + specialty + "," + availability;
     }
