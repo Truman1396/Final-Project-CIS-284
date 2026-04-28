2ec5ecae1dc8b106ff8f1e04192b58357444d833
Truman1396 <th1396@messiah.edu>
2026-04-28 14:54:49 -0400

    Update CardiologyPatient.java

--- CardiologyPatient.java
+++ CardiologyPatient.java
@@ -1,18 +1,18 @@
 public class CardiologyPatient extends Patient {
 
-    // creates a cardiology patient and 
-    //    assigns a cardiologist
+    // creates a cardiology patient (constructor)
     public CardiologyPatient(String name, int id, int age, String medicalIssue, String condition, String assignedDoctor) {
         super(name, id, age, medicalIssue, condition, assignedDoctor);
     }
 
     // returns cardiology 
-    //     patient information
+    // patient information
     @Override
     public String getInfo() {
         return "[Cardiology] " + super.getInfo();
     }
 
+    //prepares info for file writing
     public String txtPrep(){
         return name + "," + this.getId() + "," + age + "," + medicalIssue + "," + condition + "," + assignedDoctor;
     }
