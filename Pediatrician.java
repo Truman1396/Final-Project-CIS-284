96118245c1ab1ed7ec161ed2b5f05a12f54f1d61
Truman1396 <th1396@messiah.edu>
2026-04-28 14:52:43 -0400

    Update Pediatrician.java

--- Pediatrician.java
+++ Pediatrician.java
@@ -1,13 +1,16 @@
 public class Pediatrician extends MedicalProfessional {
 
+    //Constructor
     public Pediatrician(int docID, String docName, String specialty, int availability){
-
         super(docID, docName, specialty, availability);
     }
 
+    //Pediatrician action (abstract method)
     public void performAction(){
+        System.out.println(this.getDocName() + " ran vitals and gave patient a lollipop");
     }
 
+    //Prepare info for file writing
     public String txtPrep(){
         return this.getDocID() + "," + this.getDocName() + "," + specialty + "," + availability;
     }
